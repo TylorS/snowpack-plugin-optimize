79b8054556695a6ca01aeb8eaffe3ffd29669165
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-11-29 23:23:52 -0500

    fix: correct rewritten sourceMaps

--- src/writeAllFiles.ts
+++ src/writeAllFiles.ts
@@ -1,10 +1,10 @@
 import { writeFile } from 'fs/promises'
-import { EOL } from 'os'
-import { extname, relative } from 'path'
+import { dirname, extname, relative } from 'path'
 import { cyan } from 'typed-colors'
 import { info } from 'typed-figures'
 
 import { generateSourceMaps } from './generateSourceMaps'
+import { sourceMapUrl } from './getSourceMapUrl'
 
 const pNull = Promise.resolve(null)
 const sourceMapSupportedExtensions = ['.js', '.jsx', '.css']
@@ -16,23 +16,25 @@ export type WriteContent = {
   newSourceMap?: string
 }
 
-export const writeAllFiles = (log: (msg: string) => void, buildDirectory: string) => (
+export const writeAllFiles = (log: (msg: string) => void) => (
   filesToContent: ReadonlyArray<WriteContent>,
 ) =>
   Promise.all(
     filesToContent.map(async ({ filePath, oldContent, newContent, newSourceMap }) => {
       const ext = extname(filePath)
       const supportsSourceMaps = sourceMapSupportedExtensions.includes(ext)
-      const url = sourceMapUrl(buildDirectory, filePath, ext)
+      const url = sourceMapUrl(filePath, ext)
 
       // Ensure we include the sourceMap in our files
       if (supportsSourceMaps && !newContent.includes(url)) {
-        log(`${cyan(info)} Adding sourceMapUrl to ${relative(buildDirectory, filePath)}`)
+        log(`${cyan(info)} Adding sourceMapUrl to ${relative(process.cwd(), filePath)}`)
         newContent = newContent + url
       }
 
       const remapped = supportsSourceMaps
-        ? await generateSourceMaps(buildDirectory, filePath, oldContent, newContent, newSourceMap)
+        ? await generateSourceMaps(filePath, oldContent, newContent, newSourceMap).then(
+            rewriteSources(filePath),
+          )
         : null
 
       await Promise.all([
@@ -42,14 +44,12 @@ export const writeAllFiles = (log: (msg: string) => void, buildDirectory: string
     }),
   )
 
-function sourceMapUrl(buildDirectory: string, filePath: string, ext: string) {
-  return comment(ext, '/' + relative(buildDirectory, filePath) + '.map') + EOL
-}
+function rewriteSources(filePath: string) {
+  return async (sourceMap: string) => {
+    const map = JSON.parse(sourceMap)
 
-function comment(ext: string, path: string) {
-  if (ext === '.css') {
-    return `/*# sourceMappingURL=${path} */`
+    map.sources = map.sources.map((source: string) => relative(dirname(filePath), source))
+
+    return JSON.stringify(map, null, 2)
   }
-
-  return `//# sourceMappingURL=${path}`
 }
